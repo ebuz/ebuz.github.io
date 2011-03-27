44cfb3115db203d7f1a47be638869ada97c070e7
Esteban Buz <esteban.buz@gmail.com>
2011-03-27 17:49:32 -0400

    adding previously made docs
    --HG--
    branch : fancy

---
+++ fancy/scripts/doc.ready.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  
+  // set width of canvas
+  var total = $('#intro').outerWidth() + $('#about').outerWidth() + $('#school').outerWidth(true) + $('#contact').outerWidth(true);
+  $('#canvas').css({width: total });
+
+  var emailaddress = 'ebuz' + '@' + 'bcs.rochester.edu';
+  $('#email').append('<a href="mailto:'+emailaddress+'">'+emailaddress+'</a>');
+
+  // nav
+  $('ul.link li').click( function() {
+    var ele = this.className;
+    var p = $('#'+ele).offset();
+
+    //scroll
+    var x = $(window).width() - $('#'+ele).width();
+    x = Math.ceil(x/2);
+    x = p.left - x;
+    $('html,body').animate({scrollLeft: x}, 1000);
+  });
+});
